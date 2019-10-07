cda295cec4286778ff694a5fafe0c2feda97ae4b
sarah <sarahkharraz@gmail.com>
2019-10-07 12:22:52 +0100

    Create first test to see if Jest is connected #Relates Relates #64

---
+++ src/components/Menu/menu.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import Menu from './menu';
+
+afterEach(cleanup);
+
+test('Jest is working ok', () => {
+  expect(true).toBeTruthy()
+});
