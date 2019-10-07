ce71f5b7f5092f097e5a0b587673559a9d6dbdbb
frannyfra <francescadelaurentis1987@gmail.com>
2019-10-07 15:43:45 +0100

    Create a test to check if my body back logo exists Relates #35

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
 
     <header>
-        <img src={logo} alt="My Body Back Logo" />
+        <img src={logo} data-testid="MyBodyBack-logo" alt="My Body Back Logo" />
         {/* <Link to='/About'> */}
         <img src={butterfly} data-testid="butterfly-button" alt="Butterfly" />
         {/* </Link> */}
