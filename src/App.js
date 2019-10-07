9928a62cd36767828e984c89bd708702fc9d6f6d
frannyfra <francescadelaurentis1987@gmail.com>
2019-10-07 14:36:59 +0100

    Create a link connection for the butterfly Relates #65

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
 
+
 function App() {
   return (
     <div className="App">
+    
 
     <Router>
     <Route exact path="/" component={PrivacyPolicy} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,14 @@ import logo from './assets/MyBodyBackLogo.svg';
 import butterfly from './assets/Butterfly.svg';
 
 const Header = () => {
-        
+    
     return (
 
     <header>
         <img src={logo} alt="My Body Back Logo" />
+        <Link to='/About'>
         <img src={butterfly} alt="Butterfly" />
+        </Link>
     </header>
     
     )
