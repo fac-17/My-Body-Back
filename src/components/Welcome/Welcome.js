c099993c53d992412043b00209cbefc77ca10560
mkatenolan <mkatenolan@gmail.com>
2019-10-07 15:01:24 +0100

    Test to check Welcome component renders a header Relates #66

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Welcome = () => {
   return (
 <div>
-    <h1>Welcome</h1>
+    <h1 data-testid="welcome-header">Welcome</h1>
     <p> Welcome to the My Body Back app </p>
     <img src="#"/><p>This icon will take you to the "Support & Techniques" section </p>
     <img src="#"/><p>This icon will take you to the "Feeling Overwhelmed" section </p>
