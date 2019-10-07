c03e671ab721d3f5d7edb232cd6253686303f6e5
sarah <sarahkharraz@gmail.com>
2019-10-07 12:35:20 +0100

    First menu test: test if the three buttons are rendered on the document Relates #64

--- src/components/Menu/menu.test.js
+++ src/components/Menu/menu.test.js
@@ -7,3 +7,13 @@ afterEach(cleanup);
 test('Jest is working ok', () => {
   expect(true).toBeTruthy()
 });
+
+test('if the three buttons are rendered', () => {
+  const { getByTestId } = render(<Menu />);
+  // const leftButton = getByTestId('leftButton-menu')
+  // const middleButton = getByTestId('middleButton-menu')
+  // const rightButton = getByTestId('rightButton-menu')
+  expect(getByTestId('leftButton-menu')).toBeInTheDocument()
+  expect(getByTestId('middleButton-menu')).toBeInTheDocument()
+  expect(getByTestId('rightButton-menu')).toBeInTheDocument()
+})
