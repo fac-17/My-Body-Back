4d027d904afc35cd5579e7c77fcf3c27531945b2
sarah <sarahkharraz@gmail.com>
2019-10-07 12:40:57 +0100

    Setup function for the menu Relates #64

---
+++ src/components/Menu/menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Menu = () => {
+  return(
+    <nav>
+      <ul>
+        <li>left</li>
+        <li>middle</li>
+        <li>right</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu;
