58e29b80ddce0c1a7a65e61f51b2d5e1efdf89b7
mkatenolan <mkatenolan@gmail.com>
2019-10-07 17:18:24 +0100

    adjust privacy policy page styling

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -8,7 +8,8 @@ const PrivacyPolicy = () => {
     <div class="privacy__container">
     <h1 class="privacy__title"> A note about privacy </h1>
     <p class="privacy__text"> Welcome to the My Body Back app. Before you begin to explore, we want to assure you that this app does not collect any personally-identifiable information and does not use cookies to track users. </p>
-    <Link to="/welcome" ><img src={butterfly} alt="Go to welcome page"/></Link>
+    <span class="privacy__instructions"> Click the butterfly to begin. </span>
+    <Link to="/welcome" ><img class="privacy__button" src={butterfly} alt="Go to welcome page"/></Link>
     </div>
   )
 }
