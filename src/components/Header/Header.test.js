bc8daca630eed52fd97016b6f58a68b964755c38
frannyfra <francescadelaurentis1987@gmail.com>
2019-10-07 13:01:01 +0100

    Setting up the test for the Header

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,1 +1,7 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import Header from "./Header.js";
 
+test("butterfly svg exist", () => {
+    const { getByText, getByLabelText, getByTestId } = render(<Header />); 
+});
