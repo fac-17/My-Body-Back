0d2590257439348b437d8e674c0857066d77037f
frannyfra <francescadelaurentis1987@gmail.com>
2019-10-08 16:25:58 +0100

    Create a 404-not found page Relates#36

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Redirect, BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
 import Menu from "./components/Menu/Menu";
 import Welcome from "./components/Welcome/Welcome";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import FeelingOverwhelmed from "./components/FeelingOverwhelmed/FeelingOverwhelmed";
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Switch>
         <Route exact path="/" component={PrivacyPolicy} />
         <Route path="/welcome" component={Welcome} />
         <Route path="/about" component={About} />
         <Route path="/feelingoverwhelmed" component={FeelingOverwhelmed} />
+        <Route path="*" component={PageNotFound} />
+        </Switch>
       </Router>
     </div>
   );
