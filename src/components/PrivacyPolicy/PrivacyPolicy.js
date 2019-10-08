be45d4167b2c38948a150d3b577078cdbb9238b8
mkatenolan <mkatenolan@gmail.com>
2019-10-08 15:35:07 +0100

    create tests for privacy policy page and check link to page works

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -9,7 +9,7 @@ const PrivacyPolicy = () => {
     <h1 className="privacy__title"> A note about privacy </h1>
     <p className="privacy__text"> Welcome to the My Body Back app. Before you begin to explore, we want to assure you that this app does not collect any personally-identifiable information and does not use cookies to track users. </p>
     <span className="privacy__instructions"> Click the butterfly to begin. </span>
-    <Link to="/welcome" ><img className="privacy__button" src={butterfly} alt="Go to welcome page"/></Link>
+    <Link to="/welcome" ><img data-testid="butterfly-test-id" className="privacy__button" src={butterfly} alt="Go to welcome page"/></Link>
     </div>
   )
 }
