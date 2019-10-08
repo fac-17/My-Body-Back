8335599926b7fdd227fc6900bb5ecec08527cf1d
Gregor <gregorlthomson@sky.com>
2019-10-08 11:17:47 +0100

    Begin coding the component

---
+++ src/components/FeelingOverwhelmed/FeelingOverwhelmed.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./FeelingOverwhelmed.css";
+import Header from "../Header/Header";
+import Menu from "../Menu/Menu";
+
+const FeelingOverwhelmed = () => {
+  return (
+    <section id="feeling-overwhelmed__container">
+      <Header />
+      <section id="feeling-overwhelmed__body">
+        <h2 id="feeling-overwhelmed__title">Feeling Overwhelmed</h2>
+        <p>
+          This app is not about making you go for a cervical screening before
+          you are ready, but about helping you to take small steps, in your own
+          time. You may even decide that you don’t want to have a smear test at
+          all. You have the right to make this choice.
+        </p>
+      </section>
+
+      <Menu />
+    </section>
+  );
+};
