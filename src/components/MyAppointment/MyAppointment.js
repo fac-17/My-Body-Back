5bc06a8f837738a944a1d6492d61199e5ebf3006
Gregor <gregorlthomson@sky.com>
2019-10-08 16:16:45 +0100

    Correct routing Relates #63

--- src/components/MyAppointment/MyAppointment.js
+++ src/components/MyAppointment/MyAppointment.js
@@ -1,1 +1,15 @@
 import React from "react";
+import "./MyAppointment.css";
+import Header from "../Header/Header";
+import Menu from "../Menu/Menu";
+import { useSwipeable, Swipeable } from "react-swipeable";
+
+const MyAppointment = () => {
+  return (
+    <section id="my-appointment__container">
+      <p>test</p>
+    </section>
+  );
+};
+
+export default MyAppointment;
