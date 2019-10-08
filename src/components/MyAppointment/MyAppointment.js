7c769348d975fa5780bfea140a6aff37e5028cc7
mkatenolan <mkatenolan@gmail.com>
2019-10-08 17:06:43 +0100

    Add image carousel and wrote useEffect to display relevant image on swipe Relates #63 #81

--- src/components/MyAppointment/MyAppointment.js
+++ src/components/MyAppointment/MyAppointment.js
@@ -11,19 +11,36 @@ import feelingbetter from "../assets/feelingbetter.svg";
 const MyAppointment = () => {
   const [index, setIndex] = React.useState(0);
 
+  React.useEffect(
+    () => {
+      const image = document.querySelectorAll(".image-carousel");
+      const imageArray = Array.from(image);
+
+      imageArray.forEach(element => {
+        element.id == index
+          ? element.classList.add("image-carousel-active")
+          : element.classList.remove("image-carousel-active");
+      });
+    },
+    [index]
+  );
+
   const swipingLeft = () => {
-    setIndex(index + 1);
+    index < 5 ? setIndex(index + 1) : setIndex(index);
     console.log(index);
   };
 
   const swipingRight = () => {
-    setIndex(index - 1);
+    index > 0 ? setIndex(index - 1) : setIndex(index);
     console.log(index);
   };
 
   return (
     <Swipeable onSwipedLeft={swipingLeft} onSwipedRight={swipingRight}>
-      You can swipe here <p>Index is {index}</p>
+      <img className="image-carousel" id="0" src={notesOfLove} />
+      <img className="image-carousel" id="1" src={helpAndSupport} />
+      <img className="image-carousel" id="2" src={emotions} />
+      <img className="image-carousel" id="3" src={feelingbetter} />
     </Swipeable>
   );
 };
