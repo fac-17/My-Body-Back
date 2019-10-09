85d1aca68b2b79d0eaf68015ee4390c567fdc6d4
Gregor <gregorlthomson@sky.com>
2019-10-09 16:36:32 +0100

    Add second section Relates #61

--- src/components/Emotions/Emotions.js
+++ src/components/Emotions/Emotions.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Header/Header";
 import Menu from "../Menu/Menu";
 import traumaVideo from "../assets/Trauma.mp4";
+import emotionsVideo from "../assets/Emotions.mp4";
 
 const Emotions = () => {
   return (
@@ -12,13 +13,27 @@ const Emotions = () => {
         <section id="emotions__section-one">
           <h3>Why is this so difficult?!</h3>
           <figure>
-            <video width="800" height="600" controls>
+            <video width="360" height="240" controls>
               <source src={traumaVideo} type="video/mp4" />
               Your browser does not support video.
             </video>
             <figcaption>
               A video about what happens in the brain when you are faced with a
-              triggering situation, like thinking about a cervical screening
+              triggering situation, like thinking about a cervical screening.
+            </figcaption>
+          </figure>
+        </section>
+
+        <section id="emotions__section-two">
+          <h3>How our emotions work</h3>
+          <figure>
+            <video width="360" height="240" controls>
+              <source src={emotionsVideo} type="video/mp4" />
+              Your browser does not support video.
+            </video>
+            <figcaption>
+              Clinical Psychologist Dr Jane Vosper talks about the brain’s
+              emotion systems.
             </figcaption>
           </figure>
         </section>
