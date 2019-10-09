60b92c3a58f4ad36f6c379f2c4dac518d78e4eaf
Gregor <gregorlthomson@sky.com>
2019-10-09 11:58:48 +0100

    Merge with master

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import './Menu.css';
-import brain from '../assets/brain.svg';
-import wave from '../assets/wave.svg';
-import flower from '../assets/flower.svg';
+import "./Menu.css";
+import brain from "../assets/brain.svg";
+import wave from "../assets/wave.svg";
+import flower from "../assets/flower.svg";
 
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
 const Menu = () => {
-  return(
-    <nav className='menu__nav'>
+  return (
+    <nav className="menu__nav">
       <ul className="menu__list">
-      <Link to="/"><img src={brain} data-testid='leftButton-menu' className='menu__leftbutton'/></Link>
+        <Link to="/helpingourselves">
+          <img
+            src={brain}
+            data-testid="leftButton-menu"
+            className="menu__leftbutton"
+            alt="helping ourselves"
+          />
+        </Link>
         <span className="menu__circle"></span>
-        <Link to="/"><img src={wave} data-testid='middleButton-menu' className='menu__middlebutton'/></Link>
-        <Link to="/"><img src={flower} data-testid='rightButton-menu' className='menu__rightbutton'/></Link>
+        <Link to="/feelingoverwhelmed">
+          <img
+            src={wave}
+            data-testid="middleButton-menu"
+            className="menu__middlebutton"
+            alt="feeling overwhelmed"
+          />
+        </Link>
+        <Link to="/myappointment">
+          <img
+            src={flower}
+            data-testid="rightButton-menu"
+            className="menu__rightbutton"
+            alt="my appointment"
+          />
+        </Link>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Menu;
