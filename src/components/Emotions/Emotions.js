49bff6ac7d4dad3c317a202391a20a5b117a41db
Gregor <gregorlthomson@sky.com>
2019-10-09 15:37:23 +0100

    Establish sections for emotions page content Relates #61

--- src/components/Emotions/Emotions.js
+++ src/components/Emotions/Emotions.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import Header from "../Header/Header";
+import Menu from "../Menu/Menu";
 
 const Emotions = () => {
-  return(
-    <h2> Emotions </h2>
-  )
-}
+  return (
+    <section id="emotions__container">
+      <Header />
+      <section id="emotions__body">
+        <h2> Trauma, the Brain and our Emotions </h2>
+      </section>
+      <Menu />
+    </section>
+  );
+};
 
 export default Emotions;
