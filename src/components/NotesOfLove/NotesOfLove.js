bb99260cea9f47c2c767a99515f9914ed0089d1d
mkatenolan <mkatenolan@gmail.com>
2019-10-09 16:49:40 +0100

    import notes of love images Relates #67

--- src/components/NotesOfLove/NotesOfLove.js
+++ src/components/NotesOfLove/NotesOfLove.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Swipeable } from "react-swipeable";
+import noteOne from "../assets/notes-of-love/notes-of-love-one.png";
+import noteTwo from "../assets/notes-of-love/notes-of-love-two.png";
+import noteThree from "../assets/notes-of-love/notes-of-love-three.png";
+import noteFour from "../assets/notes-of-love/notes-of-love-four.png";
+import noteFive from "../assets/notes-of-love/notes-of-love-five.png";
+import noteSix from "../assets/notes-of-love/notes-of-love-six.png";
+import noteSeven from "../assets/notes-of-love/notes-of-love-seven.png";
+
 
 const NotesOfLove = () => {
 
+
 }
 
-export default NotesOfLove; 
+export default NotesOfLove;
