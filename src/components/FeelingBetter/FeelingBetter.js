e3ca24f0bb0d79499b403c75932028900a323fcd
Gregor <gregorlthomson@sky.com>
2019-10-10 12:53:44 +0100

    Initialise State for three sections Relates #62

--- src/components/FeelingBetter/FeelingBetter.js
+++ src/components/FeelingBetter/FeelingBetter.js
@@ -4,15 +4,51 @@ import Menu from "../Menu/Menu";
 import "./FeelingBetter.css";
 
 const FeelingBetter = () => {
-  return (
-    <section id="feeling-better__container">
-      <Header />
-      <section id="feeling-better__body">
-        <h2>Feeling Better</h2>
+  const [currentPage, setCurrentPage] = React.useState("home");
+
+  if (currentPage === "home") {
+    return (
+      <section id="feeling-better__container">
+        <Header />
+        <section id="feeling-better__body">
+          <h2>Feeling Better</h2>
+        </section>
+        <Menu />
       </section>
-      <Menu />
-    </section>
-  );
+    );
+  } else if (currentPage === "soothing") {
+    return (
+      <section id="feeling-better__container">
+        <Header />
+        <section id="feeling-better__body">
+          <h2>Activating your Soothing System</h2>
+        </section>
+        <Menu />
+      </section>
+    );
+  } else if (currentPage === "compassion") {
+    return (
+      <section id="feeling-better__container">
+        <Header />
+        <section id="feeling-better__body">
+          <h2>Building Self-Compassion</h2>
+        </section>
+        <Menu />
+      </section>
+    );
+  } else if (currentPage === "coping") {
+    return (
+      <section id="feeling-better__container">
+        <Header />
+        <section id="feeling-better__body">
+          <h2>
+            Tips for coping with anxiety, panic, flashbacks and dissociation
+          </h2>
+        </section>
+        <Menu />
+      </section>
+    );
+  }
 };
 
 export default FeelingBetter;
