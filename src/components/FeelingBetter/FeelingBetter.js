8b4004eead5fa24f0e0ed5ee6c4e19a01a3a2a7e
Gregor <gregorlthomson@sky.com>
2019-10-10 12:34:40 +0100

    Initialise Feeling Better Page with Menu and Header Relates #62

--- src/components/FeelingBetter/FeelingBetter.js
+++ src/components/FeelingBetter/FeelingBetter.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import Header from "../Header/Header";
+import Menu from "../Menu/Menu";
+import "./FeelingBetter.css";
 
 const FeelingBetter = () => {
-  return(
-    <h2> Feeling Better </h2>
-  )
-}
+  return (
+    <section id="feeling-better__container">
+      <Header />
+      <section id="feeling-better__body">
+        <h2>Feeling Better</h2>
+      </section>
+      <Menu />
+    </section>
+  );
+};
 
 export default FeelingBetter;
