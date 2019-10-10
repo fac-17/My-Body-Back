3f2e13031c91959dd5f6d8b5ce34b3ab0f8ce170
Gregor <gregorlthomson@sky.com>
2019-10-10 14:41:32 +0100

    Add some text for soothing partial Relates #62

---
+++ src/components/FeelingBetter/Partials/Soothing.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Soothing = () => {
+  return (
+    <section id="soothing-section__container">
+      <h3>Activating Your Soothing System</h3>
+      <section id="soothing-section-page__one">
+        <p>The soothing system calms the threat system.</p>
+        <p>
+          When the threat system is active we feel fear, anger or disgust. We
+          also feel strong physical sensations like nausea, a racing heart and
+          an urge to fight or run away. This is not our fault – it is just how
+          our minds and bodies work. But when this happens, we need to boost the
+          soothing system emotionally and physically so that we’re able to slow
+          down and feel safe, and not so overwhelmed.
+        </p>
+      </section>
+      <section id="soothing-section-page__two">
+        <p>
+          Have there been any times in the past when you were able to activate
+          your soothing system? What did you do? Perhaps you used meditation or
+          yoga, remembered to breathe… or connected with a friend or loved one?
+          Make a list of any self-soothing skills you already use.{" "}
+        </p>
+      </section>
+      <section id="soothing-section-page__three">
+        <p>
+          Before you begin the process of arranging your cervical screening,
+          consider spending some time practicing skills to activate your
+          soothing system. Thinking about the test can trigger your threat
+          system, so it is important to have these skills ready. We would
+          recommend that you try to practice the exercises on this page
+          regularly (at least once a day) for the next two weeks.
+        </p>
+      </section>
+
+      <section id="soothing-section-page__four"></section>
+    </section>
+  );
+};
+
+export default Soothing;
