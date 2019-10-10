edbc30ce5c21a44118b308fcbc1e6a431f2a83c9
sarah <sarahkharraz@gmail.com>
2019-10-10 15:59:47 +0100

    Fixed test from Privacy Policy

--- src/components/PrivacyPolicy/PrivacyPolicy.test.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { render, fireEvent, cleanup, waitForElement } from '@testing-library/react';
-import { toBeInTheDocument } from "@testing-library/jest-dom/extend-expect";
-import { BrowserRouter as Router, Route, Link, MemoryRouter } from "react-router-dom";
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { toBeInTheDocument } from '@testing-library/jest-dom/extend-expect';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { MemoryRouter} from 'react-router';
+
+import { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import PrivacyPolicy from './PrivacyPolicy';
+import App from '../../App';
 
 afterEach(cleanup);
 
@@ -13,8 +20,12 @@ afterEach(cleanup);
 describe('Test privacy policy component renders correctly', () => {
 
   test("The Privacy policy page renders", () => {
-    render(<Router><PrivacyPolicy /></Router>)
-  });
+  const component = mount(<MemoryRouter initialEntries = {['/privacypolicy']} >
+      <App />
+    </MemoryRouter>
+  );
+  expect(component.find(PrivacyPolicy)).toHaveLength(1);
+})
 
   test('Test that component renders the correct text', () => {
     const { getByText } =
