8e0ee3207c6652cfc9e7f3102ac0a0f272f604d2
Gregor <gregorlthomson@sky.com>
2019-10-14 11:47:51 +0100

    Update imports to pass tests Fixes #127

--- src/components/About/About.test.jsx
+++ src/components/About/About.test.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import {
-    render,
-    fireEvent,
-    cleanup,
-    waitForElement,
-    queryByTestid } from "@testing-library/react";
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement,
+  queryByTestid
+} from "@testing-library/react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import About from "./About.js";
+import About from "./About";
 import { toBeInTheDocument } from "@testing-library/jest-dom/extend-expect";
-
 
 afterEach(cleanup);
 
 test("Jest works ok", () => {
-    expect(true).toBeTruthy();
+  expect(true).toBeTruthy();
 });
 
 test("About component renders about page", () => {
-    const { getByTestId } = render(
-        <Router>
-            <About />
-        </Router>
-    );
-    const header = getByTestId("about-title");
-    expect(header).toBeInTheDocument();
+  const { getByTestId } = render(
+    <Router>
+      <About />
+    </Router>
+  );
+  const header = getByTestId("about-title");
+  expect(header).toBeInTheDocument();
 });
 
 test("Does the header exist in the About page?", () => {
-    const { getByTestId } = render(
-      <Router>
-        <About />
-      </Router>
-    );
-    const header = getByTestId("butterfly-header");
-    expect(header).toBeInTheDocument();
-  });
+  const { getByTestId } = render(
+    <Router>
+      <About />
+    </Router>
+  );
+  const header = getByTestId("butterfly-header");
+  expect(header).toBeInTheDocument();
+});
