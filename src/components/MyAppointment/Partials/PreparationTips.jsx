5c807471ca33417dc5f2fd4300185568a20861c8
Gregor <gregorlthomson@sky.com>
2019-10-14 16:29:24 +0100

    Refactor with ternary operator Fixes #132

--- src/components/MyAppointment/Partials/PreparationTips.jsx
+++ src/components/MyAppointment/Partials/PreparationTips.jsx
@@ -9,11 +9,9 @@ const PreparationTips = () => {
   ] = React.useState(0);
 
   const nextTip = () => {
-    if (currentPreparationTipIndex < preparationContent.length - 1) {
-      updatePreparationTipIndex(currentIndex => currentIndex + 1);
-    } else {
-      updatePreparationTipIndex(0);
-    }
+    currentPreparationTipIndex < preparationContent.length - 1
+      ? updatePreparationTipIndex(currentIndex => currentIndex + 1)
+      : updatePreparationTipIndex(0);
   };
 
   return (
