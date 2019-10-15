90aaa0ee3c4398a23d9644f816d87caa1d61b6f5
frannyfra <francescadelaurentis1987@gmail.com>
2019-10-15 13:13:40 +0100

    Add transition to componets

--- src/components/FeelingBetter/Partials/Coping.jsx
+++ src/components/FeelingBetter/Partials/Coping.jsx
@@ -1,21 +1,60 @@
 import React from "react";
+import { Swipeable } from "react-swipeable";
+import "./Coping.css";
 import groundingVideo from "../../assets/video/groundingVideo.mp4";
 import groundingAudio from "../../assets/audio/groundingAudio.mp4";
 import pmrAudio from "../../assets/audio/pmrAudio.m4a";
 
+
 const Coping = () => {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const video = document.querySelectorAll(".coping__section");
+    const videoArray = Array.from(video);
+
+    videoArray.forEach(element => {
+      console.log(videoArray)
+      element.id == index
+      ? element.classList.add(".coping__section-active")
+      : element.classList.remove(".coping__section-active");
+  });
+}, [index]);
+
+  const swipingLeft = () => {
+  index < 3 ? setIndex(index + 1) : setIndex(index);
+};
+
+const swipingRight = () => {
+  index > 0 ? setIndex(index - 1) : setIndex(index);
+};
+
+
   return (
-    <section id="coping-section__container">
-      <h3>Tips for coping with anxiety, panic, flashbacks and dissociation</h3>
-      <video controls width="300" height="200">
-        <source src={groundingVideo} type="video/mp4" />
-      </video>
+  
+  <section id="coping-section__container">
+        <h3>Tips for coping with anxiety, panic, flashbacks and dissociation</h3>
+        <Swipeable onSwipedLeft={swipingLeft} onSwipedRight={swipingRight}>
+      
+        <section className="coping__section" id="0">
+          <video controls width="300" height="200">
+            <source src={groundingVideo} type="video/mp4" />
+          </video>
+        </section>
+
+      <section className="coping__section" id="1">
       <audio controls>
         <source src={pmrAudio} type="audio/mp4" />
       </audio>
+      </section>
+
+      <section className="coping__section" id="2">
       <audio controls>
         <source src={groundingAudio} type="audio/mp4" />
       </audio>
+      </section>
+
+    </Swipeable>
     </section>
   );
 };
