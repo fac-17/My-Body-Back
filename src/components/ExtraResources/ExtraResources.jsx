8f7dfa8d960bd99e768c5a94ffbb22fe681696ba
mkatenolan <mkatenolan@gmail.com>
2019-10-15 11:51:35 +0100

    convert get data function to use async await Relates #169

--- src/components/ExtraResources/ExtraResources.jsx
+++ src/components/ExtraResources/ExtraResources.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import Menu from "../Menu/Menu";
+
 
 const ExtraResources = () => {
   const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("http://localhost:9000/getData")
-      .then(res => res.json())
-      .then(res => setData(res.records[0].fields))
-  }, []);
+    async function getInfo() {
+    const response = await fetch("http://localhost:9000/getData")
+    const json = await response.json();
+    return json;
+  }
 
+getInfo()
+.then(json => setData(json.records[0].fields))
 
-console.log(data.Link);
+}, []);
+
+// console.log("This is data", data.Link);
+
+const youtubeurl = data.Link;
+// const youtubeId = youtubeurl.split('v=', 2)[1];
+console.log("This is URL", youtubeurl);
 const youtubeId = "drJwMlD9Mjo";
 
   return(
-  <div>
+    <div>
+    <Header />
     <iframe src={`https://www.youtube.com/embed/${youtubeId}`} />
+    <Menu />
   </div>
 )
 };
