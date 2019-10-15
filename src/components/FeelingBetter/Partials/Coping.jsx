a5d22d25885f0e043dde9a1a8b2839e916f67898
frannyfra <francescadelaurentis1987@gmail.com>
2019-10-15 15:04:45 +0100

    Make page swipeable Relates#130

--- src/components/FeelingBetter/Partials/Coping.jsx
+++ src/components/FeelingBetter/Partials/Coping.jsx
@@ -7,20 +7,9 @@ import pmrAudio from "../../assets/audio/pmrAudio.m4a";
 
 
 const Coping = () => {
+  
   const [index, setIndex] = React.useState(0);
-
-  React.useEffect(() => {
-    const video = document.querySelectorAll(".coping__section");
-    const videoArray = Array.from(video);
-
-    videoArray.forEach(element => {
-      console.log(videoArray)
-      element.id == index
-      ? element.classList.add("coping__section-active")
-      : element.classList.remove("coping__section-active");
-  });
-}, [index]);
-
+ 
   const swipingLeft = () => {
   index < 3 ? setIndex(index + 1) : setIndex(index);
 };
@@ -35,25 +24,23 @@ const swipingRight = () => {
   <section id="coping-section__container">
         <h3>Tips for coping with anxiety, panic, flashbacks and dissociation</h3>
         <Swipeable onSwipedLeft={swipingLeft} onSwipedRight={swipingRight}>
-      
-        <section className="coping__section" id="0">
-          <video controls width="300" height="200">
+        <section className={index == 0  ? "coping__section-active" : "coping__section"} id="0">
+          <video controls width="300" height="200" >
             <source src={groundingVideo} type="video/mp4" />
           </video>
         </section>
 
-      <section className="coping__section" id="1">
+      <section className={index == 1  ? "coping__section-active" : "coping__section"} id="1">
       <audio controls>
         <source src={pmrAudio} type="audio/mp4" />
       </audio>
       </section>
 
-      <section className="coping__section" id="2">
+      <section className={index == 2  ? "coping__section-active" : "coping__section"} id="2">
       <audio controls>
         <source src={groundingAudio} type="audio/mp4" />
       </audio>
       </section>
-
     </Swipeable>
     </section>
   );
