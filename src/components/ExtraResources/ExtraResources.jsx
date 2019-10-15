7153fd1380c32feeec8c2cbc67f78e7532d83097
mkatenolan <mkatenolan@gmail.com>
2019-10-15 17:26:03 +0100

    map through data from Airtable and render relevant things to the extra resources page Relates #144

--- src/components/ExtraResources/ExtraResources.jsx
+++ src/components/ExtraResources/ExtraResources.jsx
@@ -2,48 +2,53 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import Menu from "../Menu/Menu";
 
-
 const ExtraResources = () => {
   const [data, setData] = React.useState(null);
   React.useMemo(() => {
     async function getInfo() {
-    const response = await fetch("http://localhost:9000/getData")
-    const json = await response.json();
-    return json;
+      const response = await fetch("http://localhost:9000/getData");
+      const json = await response.json();
+      return json;
+    }
+
+    getInfo().then(json => setData(json.records));
+  }, []);
+
+  let urlsArray = [];
+  let descriptionArray = [];
+  let titleArray = [];
+
+  if (data !== null) {
+    data.forEach(record => {
+      urlsArray.push(record.fields.Link.split("v=", 2)[1]);
+      descriptionArray.push(record.fields.Description);
+      titleArray.push(record.fields.Title);
+    });
   }
 
-getInfo()
-.then(json => setData(json.records))
-
-}, []);
-
-let urlsArray = [];
-let descriptionArray = [];
-let titleArray = [];
-
-if(data !== null){
-  data.forEach((record) => {
-  urlsArray.push(record.fields.Link);
-  descriptionArray.push(record.fields.Decription);
-  titleArray.push(record.fields.Title);
-  })
-}
-
-console.log(titleArray);
-
-const youtubeId = "drJwMlD9Mjo";
-
-    if(data === null) {
-      return(<div><h1>...loading</h1></div>)
-    } else {
-    return(
-    <div>
-    <Header />
-    <iframe src={`https://www.youtube.com/embed/${youtubeId}`} />
-    <Menu />
-  </div>
-)
-}
+  if (data === null) {
+    return (
+      <div>
+        <Header />
+        <h1>...loading</h1>
+        <Menu />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Header />
+        {titleArray.map((title, i) => (
+          <div>
+            <h2>{title}</h2>
+            <iframe src={`https://www.youtube.com/embed/${urlsArray[i]}`} />
+            <p>{descriptionArray[i]}</p>
+          </div>
+        ))}
+        <Menu />
+      </div>
+    );
+  }
 };
 
 export default ExtraResources;
