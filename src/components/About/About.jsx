238307aaf084f84861ec2c57ee319c6ccbd5a282
mkatenolan <mkatenolan@gmail.com>
2019-10-16 15:15:12 +0100

    adjust about page styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./About.css";
-import Menu from "../Menu/Menu";
 import Header from "../Header/Header";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -13,21 +12,21 @@ const About = () => {
         <h2 className="about__title">About this App</h2>
         <p className="about__text__main">
           This app is not about making you go for a cervical screening before
-          you are ready, but about helping you to take small steps, in your own
-          time. You may even decide that you don’t want to have a smear test at
+          you are ready, but about helping you to take <span className="about__text__bold"> small steps, in your own
+          time. </span>  You may even decide that you don’t want to have a smear test at
           all. You have the right to make this choice.
         </p>
         <p className="about__text__main">
-          Our app is all about helping you to be more compassionate towards
-          yourself.
+          Our app is all about helping you to be  <span className="about__text__bold"> more compassionate towards
+          yourself. </span>
         </p>
         <p className="about__text__main">
-          Many people who have experienced sexual assault find the thought of
-          going for a smear test very distressing. Receiving reminder letters in
+          Many people who have experienced sexual assault <span className="about__text__bold"> find the thought of
+          going for a smear test very distressing. </span> Receiving reminder letters in
           the post and being asked why you haven’t had a screening can make
           people feel ashamed, embarrassed and uncomfortable. If you have felt
-          this way, then please remember that it is not your fault – other
-          people find this hard too.
+          this way, then please <span className="about__text__bold"> remember that it is not your fault – other
+          people find this hard too. </span> 
         </p>
 
         <p className="about__text__main">Everything about</p>
@@ -36,7 +35,6 @@ const About = () => {
         </Link>
       </div>
 
-      <Menu />
     </div>
   );
 };
