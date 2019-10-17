dd06636411342d5839fdbfaa5c5641efa4c7ba9d
frannyfra <francescadelaurentis1987@gmail.com>
2019-10-17 21:16:18 +0100

    Correct content on welcome page

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -36,7 +36,7 @@ const Welcome = ({ setMenuDisplay }) => {
       <div className="welcome__icon__container">
         <img className="welcome__icon" src={flower} alt="flower"/>
         <p className="welcome__text">
-           My Appointment section will help you to get prepare and open yourself, as a flower to professionals which will guide through in the path of acceptance and feeling better.
+           My Appointment section will help you to get prepared and open yourself, as a flower to professionals which will guide through in the path of acceptance and feeling better.
         </p>
       </div>
       <div className="welcome__icon__container">
